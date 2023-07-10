bfcd8231e83a71e66913fa3eaf928a46a7621a9c
Jorge Garcia Casalett <jgarciac75@gmail.com>
2023-07-10 13:01:57 -0500

    Task 1.1 Paso 1 Part 2

--- Tasks/Part1/courseinfo/src/index.js
+++ Tasks/Part1/courseinfo/src/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header =(props)=>{
+ return(
+  <h1>{props.course}</h1>
+ )
+}
+
+const Parts = (props)=>{
+  return(
+    <p>
+    {props.part} {props.exercises}
+  </p>
+  )
+}
+
+const Content = (props)=>{
+  return(
+    <>
+      <Parts part={props.part1} exercises={props.exercises1} />
+      <Parts part={props.part2} exercises={props.exercises2} />
+      <Parts part={props.part3} exercises={props.exercises3} />
+    </>
+  )
+}
+
+const Total = (props)=>{
+  return(
+    <div>
+      <p>
+        <p>Number of Exercises {props.total}</p>
+      </p>
+    </div>
+  )
+}
+
 const App = () =>{
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,21 +45,20 @@ const App = () =>{
   const exercises3 = 14
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>
-        <p>Number of Exercises {exercises1+exercises2+exercises3}</p>
-      </p>
-    </div>
+    <>
+      <Header course={course} />
+      <Content
+          part1={part1}
+          exercises1={exercises1}
+          part2={part2}
+          exercises2={exercises2}
+          part3={part3}
+          exercises3={exercises3}
+        />
+      <Total
+          total={exercises1+exercises2+exercises3}
+      />
+    </>
   )
 }
 
